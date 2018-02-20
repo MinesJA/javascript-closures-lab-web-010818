ab8f85d51e084b2f756b67e53db43621e268fb7b
MinesJA <minesja@gmail.com>
2018-02-20 16:30:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+
+function bumpCounter(){
+  var counter = 0;
+
+  function addBump(){
+    counter ++;
+  }
+
+  function getBumps(){
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType){
+
+  function innerFunction(deadlyDevice){
+    let newObj = {};
+
+    newObj.animalType = animalType;
+    newObj.deadlyDevice = deadlyDevice;
+
+    return newObj;
+  };
+
+  return innerFunction;
+}
+
+var sharkCreator = createAnimal("Shark");
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam");
+var sharkWithFrickinCannon = sharkCreator("Cannon");
